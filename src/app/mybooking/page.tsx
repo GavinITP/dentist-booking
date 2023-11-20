b81cabc22d06fa832bea7f1806875071f29fc72b
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-11-20 21:23:53 +0700

    heading of booking history page

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -1,5 +1,17 @@
+import { Box, Heading } from "@chakra-ui/react";
+
 const MyBooking = () => {
-  return <div>MyBooking</div>;
+  return (
+    <Box textAlign={"center"}>
+      <Heading
+        fontSize={{ base: "2xl", sm: "4xl" }}
+        fontWeight={"extrabold"}
+        my={16}
+      >
+        My Booking
+      </Heading>
+    </Box>
+  );
 };
 
 export default MyBooking;
