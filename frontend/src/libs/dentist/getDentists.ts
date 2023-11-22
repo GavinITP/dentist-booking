2e9f00fb8e54443f5485bbceb205db95211e0f2e
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-11-22 12:34:22 +0700

    getDentists no cache

--- frontend/src/libs/dentist/getDentists.ts
+++ frontend/src/libs/dentist/getDentists.ts
@@ -1,5 +1,7 @@
 export default async function getDentists() {
-  const res = await fetch("http://localhost:9000/api/v1/dentists");
+  const res = await fetch("http://localhost:9000/api/v1/dentists", {
+    cache: "no-cache",
+  });
 
   if (!res.ok) throw new Error("Failed to fetch dentists");
 
