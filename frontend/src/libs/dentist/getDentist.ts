1aaa1b0f2c8d01a9e079987c2c3e3e2fa6a18cc5
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-11-22 12:11:30 +0700

    make admin can create dentist profile

--- frontend/src/libs/dentist/getDentist.ts
+++
@@ -1,7 +0,0 @@
-export default async function getDentist(id: string) {
-  const res = await fetch("http://localhost:9000/api/v1/dentists/" + id);
-
-  if (!res.ok) throw new Error("Failed to fetch a dentist");
-
-  return res.json();
-}
